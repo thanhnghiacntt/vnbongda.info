009673e82d38d3c13273fcf0aa2227e89f29eca8
Nguyễn Thành Nghĩa <nghiant@dekiconnect.com>
2018-07-24 17:34:21 +0700

    Update source

---
+++ app/Http/Controllers/API/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class HomeController extends BaseController {
+    
+    public function __construct()
+    {
+    }
+    
+    public function getInfo(){
+        return "Helloword";
+    }
+}
